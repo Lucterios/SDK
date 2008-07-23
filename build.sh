90bdd47ad58c4fd9f02e786775a8151213974da1
user <user@69e63612-969a-4371-9866-b14aae95a0cb>
2008-07-23 18:16:25 +0000

    git-svn-id: http://projets.lucterios.org/svn/Clients/SDK@57 69e63612-969a-4371-9866-b14aae95a0cb

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+svn checkout http://projets.lucterios.org/svn/Clients/SDK/ . --username=user --password=user123
+
+rm -f *.zip
+rm -f *.tar.gz
+rm -f *.tar
+rm -f *.lpk
+rm -f Output.txt
+
+VersionMaj=0
+VersionMin=15
+VersionRev=2
+current_date_sec=`date +%s`
+VersionBuild=$(( (current_date_sec-1214866800)/7200 ))
+
+clear
+
+echo "*** Compilation $VersionMaj.$VersionMin.$VersionRev.$VersionBuild ***"
+echo $VersionMaj $VersionMin $VersionRev $VersionBuild > version.txt
+
+rm -f setup.inc.php
+cp template_setup.inc.php setup.tmp
+sed -e "s/\$version_max=X;/\$version_max=$VersionMaj;/" setup.tmp > setup.inc.php
+cp setup.inc.php setup.tmp
+sed -e "s/\$version_min=X;/\$version_min=$VersionMin;/" setup.tmp > setup.inc.php
+cp setup.inc.php setup.tmp
+sed -e "s/\$version_release=X;/\$version_release=$VersionRev;/" setup.tmp > setup.inc.php
+cp setup.inc.php setup.tmp
+sed -e "s/\$version_build=X;/\$version_build=$VersionBuild;/" setup.tmp > setup.inc.php
+
+ArcFileName=JDK_$VersionMaj-$VersionMin-$VersionRev-$VersionBuild
+
+tar -cf $ArcFileName.tar Actions Backup/readme Class CNX/readme Help images Main tutorials ConnectionSDK.inc.php coreIndex.php DeleteModule.php FunctionTool.inc.php GNU_General_Public_License.txt Help.php index.php PathInitial.inc.php ReloadModule.php setup.inc.php
+if [ $? -ne 0 ]
+then
+	echo "+++ Error lpk +++"
+	exit 2
+fi
+cp $ArcFileName.tar JDK
+gzip -f -S .lpk JDK
+if [ $? -ne 0 ]
+then
+	echo "+++ Error lpk +++"
+	exit 2
+fi
+
+echo "--- Compilation $VersionMaj.$VersionMin.$VersionRev.$VersionBuild Success ---"
+
+cp antBuilderOutput.log Output.txt
---
+++ template_setup.inc.php
@@ -0,0 +1,45 @@
+<?php
+// 
+//     This file is part of Lucterios.
+// 
+//     Lucterios is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation; either version 2 of the License, or
+//     (at your option) any later version.
+// 
+//     Lucterios is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with Lucterios; if not, write to the Free Software
+//     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+// 	Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
+//  // setup file write by SDK tool
+// --- Last modification: Date 01 March 2008 12:28:58 By  ---
+
+$extention_name="SDK";
+$extention_description="Software Developpement Kit pour Lucterios.{[newline]}Permet de créer et modifier de nouveaux modules.";
+$extention_appli="";
+$extention_famille="client";
+$extention_titre="Lucterios SDK";
+$extension_libre=true;
+
+$version_max=X;
+$version_min=X;
+$version_release=X;
+$version_build=X;
+
+$depencies=array();
+$depencies[0] = new Param_Depencies("CORE", 0, 15, 0, 15, false);
+
+$rights=array();
+
+$menus=array();
+
+$actions=array();
+
+$params=array();
+?>
