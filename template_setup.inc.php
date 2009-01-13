767f7be9ecc3d3325fb75994ffcc30d7407e0979
user <user@69e63612-969a-4371-9866-b14aae95a0cb>
2009-01-13 22:05:27 +0000

    Nouvelle version 0.90
    git-svn-id: http://projets.lucterios.org/svn/Clients/SDK@319 69e63612-969a-4371-9866-b14aae95a0cb

--- template_setup.inc.php
+++ template_setup.inc.php
@@ -33,7 +33,7 @@ $version_release=X;
 $version_build=X;
 
 $depencies=array();
-$depencies[0] = new Param_Depencies("CORE", 0, 16, 0, 16, false);
+$depencies[0] = new Param_Depencies("CORE", 0, 90, 0, 90, false);
 
 $rights=array();
 
