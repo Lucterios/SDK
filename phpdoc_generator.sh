e1c5ffff95b4635bc91b44afa5de35c87a4b2cbe
Laurent GAY <l.gay@sd-libre.fr>
2013-05-22 12:10:53 +0200

    Documentation

--- phpdoc_generator.sh
+++ phpdoc_generator.sh
@@ -12,6 +12,6 @@ PATH_PROJECT=./tutorials/
 PATH_DOCS=./Help
 RIC=./GNU_General_Public_License.txt,./LisezMoi
 
-phpdoc -f $FILE_PROJECT -d $PATH_PROJECT -t $PATH_DOCS -ti $TITLE -dn $PACKAGES -o HTML:frames:default -ric $RIC 
+phpdoc -f $FILE_PROJECT -d $PATH_PROJECT -t $PATH_DOCS -ti $TITLE -dn $PACKAGES -o HTML:Smarty:PHP -ric $RIC 
 
 #phpdoc -f $FILE_PROJECT -d $PATH_PROJECT -t $PATH_DOCS -ti $TITLE -dn $PACKAGES -o PDF:default:default -p on -ric $RIC 
